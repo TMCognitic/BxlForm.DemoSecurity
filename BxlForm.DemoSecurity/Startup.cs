9119d33c5b9060fcb1f4381210e73fff0fe5e5d6
Morre Thierry <Briareos@live.be>
2021-06-25 11:03:06 +0200

    Fixe Route (Declare Area Route beafore Default Route)

--- BxlForm.DemoSecurity/Startup.cs
+++ BxlForm.DemoSecurity/Startup.cs
@@ -75,13 +75,12 @@ namespace BxlForm.DemoSecurity
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapControllerRoute(
-                    name: "areas",
-                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-          );
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");                
             });
         }
     }
